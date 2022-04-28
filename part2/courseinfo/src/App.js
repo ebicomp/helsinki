7682f8918f8a733b45e54f86994658c8e735e0ac
Abolfazl Karimi <ab.karimi@cbi.ir>
2022-04-28 15:06:12 +0430

    initial commit of the part2 project

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,89 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
+);
+const Course = ({ course }) => {
+  const { name, parts } = course;
+
+  const total = parts.reduce((previous, current) => {
+    return previous.exercises + current.exercises;
+  });
+
+  return (
+    <div>
+      <Header course={name} />
+      <Content parts={parts} />
+      <Total sum={total} />
+    </div>
+  );
+};
+
+const App = () => {
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
+  //return <Course course={courses[0]} />;
+  return (
+    <div>
+      {courses.map((course) => {
+        return <Course key={course.id} course={course} />;
+      })}
+    </div>
+  );
+};
+
+export default App;
